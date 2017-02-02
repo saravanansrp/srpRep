36eb8d75b4a8055ff1e96f5a6d1ee65ee83f9cce
saravanansrp <saravanansrp@hotmail.com>
2017-02-02 20:12:37 +0530

    Adding multi select
    Adding multi select

--- js/custom/table.js
+++ js/custom/table.js
@@ -1,6 +1,53 @@
 var app = angular.module("nielsenLostBasketApp", []);
 app.controller("tableController", function($scope, $http) {
-	$http.get('data.json').success(function(data) {
-		$scope.personalDetails = data;
-	});
+	$http.get('data.json').success(
+			function(data) {
+				$scope.personalDetails = data;
+				$scope.categorySelects = [ 'Category A', 'Category B',
+						'Category C' ];
+				$scope.category = [];
+				$scope.dateRangeSelects = [ 'Full Year 2017', 'Latest Month',
+						'Latest 52 Weeks', 'Latest 26 Weeks',
+						'Year Ago 52 Weeks', 'Year Ago 4 Weeks',
+						'Year Ago 26 Weeks' ];
+				$scope.dateRangeSelectval = '';
+				$scope.geographySelects = [ 'Africa', 'Asia', 'Australia',
+						'Europe', 'USA' ];
+				$scope.geography = '';
+			});
+	$scope.results = [];
+	$scope.optimizedSearch = function(personalDetail) {
+		return personalDetail.User.Stats[0].active === "1";
+	};
+
+	$scope.tableEnabled = true;
+	$scope.chartEnabled = false;
+
+	$scope.simulatorEnabled = true;
+	$scope.optimizerEnabled = false;
+
+	$scope.enableTable = function() {
+		$scope.tableEnabled = true;
+		$scope.chartEnabled = false;
+	};
+
+	$scope.enableChart = function() {
+		$scope.tableEnabled = false;
+		$scope.chartEnabled = true;
+	};
+
+	$scope.enableSimulator = function() {
+		$scope.optimizerEnabled = false;
+		$scope.simulatorEnabled = true;
+	};
+
+	$scope.enableOptimizer = function() {
+		$scope.optimizerEnabled = true;
+		$scope.simulatorEnabled = false;
+	};
+	
+	 $scope.filterByCategory = function(personalDetail) {
+	        return ($scope.category.indexOf(personalDetail.category) !== -1);
+	    };	
+	
 });
